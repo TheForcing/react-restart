ec02a7fee33ab82a5e849e08c80370e12b2632c4
박지훈 <qkrw6@naver.com>
2023-06-22 16:00:22 +0900

    day2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const name= '리액트';
+ const style={
+  // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
+  backgroundColor: 'black',
+  color: 'aqua',
+  fontSize: '48px', //font=size -> fontSize
+  fontWeight: 'bold', //font-weight -> fontWeight
+  padding: 16// 단위를 생략하면 px로 지정됩니다.
+};
+return <div style={style}>}name</div>;
 }
-
 export default App;
