08f447eca1b682e1d5f6c43c8868ebec00367c2f
박지훈 <qkrw6@naver.com>
2023-06-22 17:07:44 +0900

    props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './App.css';
+import MyComponent from './MyComponent';
 
-function App() {
- const name= '리액트';
- const style={
-  // background-color는 backgroundColor와 같이 -가 사라지고 카멜 표기법으로 작성됩니다.
-  backgroundColor: 'black',
-  color: 'aqua',
-  fontSize: '48px', //font=size -> fontSize
-  fontWeight: 'bold', //font-weight -> fontWeight
-  padding: 16// 단위를 생략하면 px로 지정됩니다.
-};
-return <div style={style}>}name</div>;
+const App = () => {
+  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+
 }
+
 export default App;
