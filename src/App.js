70ff47e16dcbe554a9f1d328a2645ef792793dec
박지훈 <qkrw6@naver.com>
2023-06-24 15:20:48 +0900

    lifecycle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import { render } from '@testing-library/react';
-import InterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+
+  state= {
+    color : '#000000'
+  }
+  
+  handleClick= () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
+
   render(){
     return(
-      <InterationSample/>
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
     );
   }
 }
