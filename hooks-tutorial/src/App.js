337d3b10598313c6a5fe803ef4e79d177e808198
박지훈 <qkrw6@naver.com>
2023-06-26 18:27:34 +0900

    hooks

---
+++ hooks-tutorial/src/App.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+
+const App = () => {
+    return <Info/>;
+           
+};
+
+export default App;
