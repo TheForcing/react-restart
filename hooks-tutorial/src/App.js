0fe9e62e0f92a798dcfabcc10c0d37799979e8a2
박지훈 <qkrw6@naver.com>
2023-06-26 18:51:04 +0900

    useMemo

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,9 +1,7 @@
 import React, {useState} from "react";
-import Counter from "./Counter";
-import Info from "./Info";
-
+import Average from "./Average";
 const App = () => {
-    return <Info/>;
+    return <Average/>;
            
 };
 
