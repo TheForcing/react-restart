c80b64e1c7274d5e2361d1d3d162214320d361d5
박지훈 <qkrw6@naver.com>
2023-06-27 22:51:57 +0900

    reactHooks

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,24 +1,13 @@
-import React, {useReducer} from "react";
-
-function reducer(state, action){
-    return{
-        ...state ,
-        [action.name]: action.value
-    };
-};
-
+import React from "react";
+import useInputs from "./useInputes";
 
 const Info = () =>{
     
-    const [ state, dispatch] = useReducer(reducer, {
-        name: '',
-        nickname: ''
-    });
-
-    const { name, nickname } = state;
-    const onChange = e => {
-        dispatch(e.target);
-    };
+   const [ state, onChange] = useInputs ({
+    name: '',
+    nickname: ''
+   });
+   const { name, nickname } = state; 
 
     return (
         <div>
