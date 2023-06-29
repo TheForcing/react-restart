adf00d5e63b9e3bcb14664533ac4eae4140f199b
박지훈 <qkrw6@naver.com>
2023-06-29 17:21:10 +0900

    todo List

---
+++ todo-app/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+const App = () =>{
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: `내가 하고싶은 것`,
+      checked: true,
+    },
+    {
+      id:2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id:3,
+      text: '일정 관리 앱 만들어 보기',
+      checked: false,
+    },
+  ]);
+
+  return <TodoTemplate>
+       <TodoInsert/>
+       <TodoList todos={todos}/>
+  </TodoTemplate>;
+};
+
+
+
+export default App;
