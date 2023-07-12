0232e6fa23b57c8bd04b9aba303b09b68dc64838
박지훈 <qkrw6@naver.com>
2023-07-12 19:06:40 +0900

    react redux

---
+++ react-redux-tutorial/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react';
+import { composeWithDevTools} from 'redux-devtools-extension';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root'),
+);
+
+
+reportWebVitals.unregister();
