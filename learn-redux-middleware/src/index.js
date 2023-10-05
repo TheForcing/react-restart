ee01e9c60b5c2b0d415a4523ef3725b33291393d
박지훈 <qkrw6@naver.com>
2023-10-05 15:04:14 +0900

    Commit before SSR

--- learn-redux-middleware/src/index.js
+++ learn-redux-middleware/src/index.js
@@ -9,10 +9,10 @@ import rootReducer, { rootSaga } from './modules';
 import { createLogger} from 'redux-logger'
 import ReduxThunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga'
-
+import { composeWithDevTools } from '@reduxjs/toolkit/dist/devtoolsExtension';
 const logger = createLogger();
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk, sagaMiddleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
